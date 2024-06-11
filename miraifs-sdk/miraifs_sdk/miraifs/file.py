9d8d420668bcbb218484299bb1b198f663daf1a9
Brian Li <brian@brianli.com>
2024-06-11 21:44:23 +0900

    Work on refactor.

--- miraifs-sdk/miraifs_sdk/miraifs/file.py
+++ miraifs-sdk/miraifs_sdk/miraifs/file.py
@@ -1,5 +1,6 @@
 from miraifs_sdk.miraifs import MiraiFs
 from pysui import handle_result
+from pysui.sui.sui_builders.get_builders import GetDynamicFieldObject
 from pysui.sui.sui_txn.sync_transaction import SuiTransaction
 from pysui.sui.sui_txresults.complex_tx import TxResponse
 from pysui.sui.sui_txresults.single_tx import ObjectRead
@@ -239,3 +240,20 @@ class MfsFile(MiraiFs):
                 )
 
         return register_file_chunk_caps
+
+    def get_create_image_chunk_cap_ids_for_file(
+        self,
+    ) -> list[str]:
+        builder = GetDynamicFieldObject(
+            parent_object_id=ObjectID(self.file_id),
+            name={
+                "type": "0x1::string::String",
+                "value": "create_file_chunk_cap_ids",
+            },
+        )
+        result = handle_result(self.client.execute(builder))
+
+        if not isinstance(result, ObjectRead):
+            raise Exception("Failed to get create file chunk cap ids.")
+
+        return result.content.fields["value"]["fields"]["contents"]
--- miraifs-sdk/miraifs_sdk/miraifs/file_chunk.py
+++ miraifs-sdk/miraifs_sdk/miraifs/file_chunk.py
@@ -10,6 +10,7 @@ from pysui.sui.sui_txresults.single_tx import ObjectRead
 from pysui.sui.sui_types import (
     ObjectID,
     SuiAddress,
+    SuiBoolean,
     SuiString,
     SuiU8,
     SuiU16,
@@ -28,7 +29,7 @@ from miraifs_sdk.models import (
     CreateFileChunkCap,
     RegisterFileChunkCap,
 )
-from miraifs_sdk.file import MfsFile
+from miraifs_sdk.miraifs.file import MfsFile
 
 
 class MfsFileChunk(MfsFile):
@@ -40,6 +41,42 @@ class MfsFileChunk(MfsFile):
         super().__init__()
         self.file_id = file_id
         self.file_chunk_id = file_chunk_id
+
+    def create(
+        self,
+        create_file_chunk_cap: CreateFileChunkCap,
+        chunk: list[str],
+        verify_hash_onchain: bool,
+        gas_coin: GasCoin | None = None,
+    ) -> TxResponse:
+        txer = SuiTransaction(
+            client=self.client,
+            compress_inputs=True,
+        )
+
+        chunk_vector = txer.make_move_vector(
+            items=[SuiString(subchunk) for subchunk in chunk],
+            item_type="0x1::string::String",
+        )
+
+        txer.move_call(
+            target=f"{PACKAGE_ID}::file::create_file_chunk",
+            arguments=[
+                ObjectID(create_file_chunk_cap.id),
+                chunk_vector,
+                SuiBoolean(verify_hash_onchain),
+            ],
+        )
+
+        result = handle_result(
+            txer.execute(
+                gas_budget=5_000_000_000,
+                use_gas_object=gas_coin.id if gas_coin else None,
+            ),
+        )
+
+        if isinstance(result, TxResponse):
+            return result
 
     def view(
         self,
@@ -60,3 +97,18 @@ class MfsFileChunk(MfsFile):
         )
 
         return file_chunk
+
+    def get_create_image_chunk_cap(
+        self,
+        object_id: str,
+    ) -> CreateFileChunkCap:
+        result = handle_result(
+            self.client.get_object(ObjectID(object_id)),
+        )
+
+        if isinstance(result, ObjectRead):
+            return CreateFileChunkCap(
+                id=result.object_id,
+                hash=result.content.fields["hash"],
+                file_id=result.content.fields["file_id"],
+            )
